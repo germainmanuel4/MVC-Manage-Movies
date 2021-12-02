a761af8ecba7c8a074fe79787d409486e8ea7fbe
germainmanuel4 <62697182+germainmanuel4@users.noreply.github.com>
2021-12-02 19:18:58 +0800

    MVC Visual Studio 2022 / Manage-Movies-App
    including:
    MVC
    Visual Studio 2022
    SQL
    Validation server/client
    C#

---
+++ MVCCRUDVisualStudio2022/Controllers/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCCRUDVisualStudio2022.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       //A Controller class is where the code is written that handles the incoming browser requests.
+
+        //GET: /HelloWorld/
+        /*public string Index() //THis index is converted to HelloWorld endpoint
+        {
+            return "This is my default action...";
+        }*/
+
+        //GET /HelloWorld/Welcome
+
+        /*public string Welcome(string name, int ID = 1) 
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+
+        }
+
+
+    }
+}
---
+++ MVCCRUDVisualStudio2022/Data/MVCCRUDVisualStudio2022Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUDVisualStudio2022.Models;
+
+namespace MVCCRUDVisualStudio2022.Data
+{
+    public class MVCCRUDVisualStudio2022Context : DbContext
+    {
+        public MVCCRUDVisualStudio2022Context (DbContextOptions<MVCCRUDVisualStudio2022Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCRUDVisualStudio2022.Models.Movie> Movie { get; set; }
+    }
+}
